72e0b53e09f5d4bc3028380fc771214ff5d30ba6
aidenkael <aidenkael686@gmail.com>
2026-08-25 18:30:22 +0800

    修复工作台本地启动加载

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['sharp'],
+  allowedDevOrigins: ['127.0.0.1'],
 };
 
 export default nextConfig;
